7338120f803ef8a0ef1eddb5dd9564f0203ba2f7
hamza3256 <hamza3256@live.co.uk>
2022-01-02 19:52:36 +0000

    Updates

---
+++ precache-manifest.4ca0f4260a295932339231dde8b17f11.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2f1ce573829a85a3e65d3338e774005a",
+    "url": "/hamza3256.github.io/index.html"
+  },
+  {
+    "revision": "36ecfb1f2809c99bf544",
+    "url": "/hamza3256.github.io/static/css/main.af6d9b4f.chunk.css"
+  },
+  {
+    "revision": "ab7d973f684b43831b24",
+    "url": "/hamza3256.github.io/static/js/2.6d1bcdd8.chunk.js"
+  },
+  {
+    "revision": "c87e50d81cc7b5311525cc6fd5482ea5",
+    "url": "/hamza3256.github.io/static/js/2.6d1bcdd8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "36ecfb1f2809c99bf544",
+    "url": "/hamza3256.github.io/static/js/main.adc79062.chunk.js"
+  },
+  {
+    "revision": "67a00de495bf7e74992f",
+    "url": "/hamza3256.github.io/static/js/runtime-main.ead79e63.js"
+  },
+  {
+    "revision": "cfee3159d4f49e5105404f722a7fe587",
+    "url": "/hamza3256.github.io/static/media/Resume__Muhammad_Hamza.cfee3159.pdf"
+  },
+  {
+    "revision": "d78ebf92e4db1e60fa9779f655236bed",
+    "url": "/hamza3256.github.io/static/media/myProf.d78ebf92.jpg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.8315faa0eb2e19ca2e7ad1b8aecaef16.js"
+  "/hamza3256.github.io/precache-manifest.4ca0f4260a295932339231dde8b17f11.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/hamza3256.github.io/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
