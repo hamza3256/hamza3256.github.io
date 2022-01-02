f7efa5ea017718365ae1de0342e6a972907a0e49
hamza3256 <hamza3256@live.co.uk>
2022-01-02 17:50:49 +0000

    Updates

---
+++ precache-manifest.b0608c60380d7e783e6df4570781bf49.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f4e7307a5d25d08879316fe20cfb869e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "fe8af6c6a518dfdc6413",
+    "url": "/static/css/main.af6d9b4f.chunk.css"
+  },
+  {
+    "revision": "5158e6fe966288125ae3",
+    "url": "/static/js/2.900d0616.chunk.js"
+  },
+  {
+    "revision": "c87e50d81cc7b5311525cc6fd5482ea5",
+    "url": "/static/js/2.900d0616.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fe8af6c6a518dfdc6413",
+    "url": "/static/js/main.9c6d63c8.chunk.js"
+  },
+  {
+    "revision": "ca87d4764bfbadb807ee",
+    "url": "/static/js/runtime-main.bf0b46b0.js"
+  },
+  {
+    "revision": "cfee3159d4f49e5105404f722a7fe587",
+    "url": "/static/media/Resume__Muhammad_Hamza.cfee3159.pdf"
+  },
+  {
+    "revision": "d78ebf92e4db1e60fa9779f655236bed",
+    "url": "/static/media/myProf.d78ebf92.jpg"
+  }
+]);
